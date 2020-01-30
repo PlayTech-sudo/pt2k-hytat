eb7c5c799c4671b00b446c9bf170df8acce79461
Apeksha M Katigi <Apekshakatigi@gmail.com>
2020-01-30 11:45:27 +0530

    Updated parts module

--- hyt/parts.php
+++ hyt/parts.php
@@ -1,8 +1,8 @@
 <?php
 	session_start();
 	// ob_start(ob_gzhandler);
-	$title = "Add Project";
-	$acc_code = "D01";
+	$title = "Add Part";
+	$acc_code = "G02";
 	require "./functions/access.php";
 	require_once "./template/header.php";
 	require_once "./template/sidebar.php";
@@ -13,27 +13,18 @@
 <div class="content" style="min-height: calc(100vh - 160px);">
 	<div class="container-fluid">
 		<div class="row">
-		  	<div class="col-lg-2 col-md-2">
-		    	<ul class="nav nav-pills nav-pills-rose nav-pills-icons flex-column" role="tablist">
-		      		<li class="nav-item">
-		        		<a class="nav-link active show" data-toggle="tab" href="#usr1" role="tablist">
-		          			<i class="material-icons">add</i> ADD 
-		        		</a>
-		      		</li>
-		      		
-		    	</ul>
-		  	</div>
 		  	
-		      				<div class="col-md-5 col-sm-12">
+		  	
+		      				<div class="col-md-4 col-sm-12">
 						        <div class="card">
 						          	<div class="card-header card-header-rose card-header-icon">
 						              	<div class="card-icon">
 						                	<i class="material-icons">add</i>
 						              	</div>
-						              	<h4 class="card-title">Add Parts</h4>
+						              	<h4 class="card-title">Add Part</h4>
 						          	</div>
 						          	<div class="card-body">
-						            	<form name="form4" action="process/admin/part_process.php" method="POST">
+						            	<form name="form4" action="parts.php" method="POST">
 						              		<div class="form-group bmd-form-group">
 						                      	<label class="bmd-label-floating">Parts ID</label>
 						                      	<input type="text" class="form-control" id="part_id" name="part_id" required="" autofocus="">
@@ -46,10 +37,28 @@
 						                      	<label class="bmd-label-floating"> Parts Type</label>
 						                     	<input type="text" class="form-control" id="part_type" required="" name="part_type">
 						                  	</div>
+						                  	<div class="form-group bmd-form-group">
+						                      	<label class="bmd-label-floating"> Prod id</label>
+						                     	<input type="text" class="form-control" id="prod_id" required="" name="prod_id">
+						                  	</div><div class="form-group bmd-form-group">
+						                      	<label class="bmd-label-floating"> Parts amt</label>
+						                     	<input type="text" class="form-control" id="part_amt" required="" name="part_amt">
+						                  	</div>
 						                  	
 						                  	<div class="row">
 						                    	<div class="col-md-12">
 						                      		<button class="btn btn-success" name="add" type="submit">Add</button>
+						                      		<?php
+						                      		if (isset($_POST['add'])) {
+														$sql = "INSERT INTO parts (part_id, part_name, part_type,prod_id,part_amt) VALUES ('".$_POST["part_id"]."','".$_POST["part_name"]."','".$_POST["part_type"]."','".$_POST["prod_id"]."','".$_POST["part_amt"]."')";
+														if (mysqli_query($conn, $sql)) {
+															echo "<script type='text/javascript'>showNotification('top','right','Record Added Successfully.','info');</script>";
+														} else {
+														     echo "<script type='text/javascript'>showNotification('top','right','Duplicate Entry.','info');</script>";
+														}
+
+													}
+						                      		?>
 						                      		<button class="btn btn-danger" type="reset">Clear</button>
 						                    	</div>
 						                  	</div>
@@ -58,7 +67,7 @@
 						        </div>
 						    </div>
 						    
-		      				<div class="col-md-5 col-sm-12">
+		      				<div class="col-md-8 col-sm-12">
 		      					<div class="card">
 								  	<div class="card-header card-header-rose card-header-icon">
 								    	<div class="card-icon">
@@ -74,30 +83,34 @@
 											            <th class="text-center">Part_ID</th>
 											            <th> Name</th>
 											            <th> Type</th>
-											            <th> </th>
+											            <th> prod_id </th>
+											            <th> part_amt </th>
 											     	<th colspan="2" align="center">             action </th>
 								          			</tr> 
 								        		</thead>
 								        		<tbody>
+
 								        			<?php
-								        				$res = getData($conn, "part");
-								        				foreach ($res as $part) {
+								        				$res = getData($conn, "parts");
+								        				foreach ($res as $parts) {
 								        			?>
 									          		<tr>
-											            <td class="text-center"><?php echo $project['part_id']; ?></td>
-											            <td><?php echo $project['part_name']; ?></td>
-											            <td><?php echo $project['part_type']; ?></td>
+											            <td class="text-center"><?php echo $parts['part_id']; ?></td>
+											            <td><?php echo $parts['part_name']; ?></td>
+											            <td><?php echo $parts['part_type']; ?></td>
+											            <td><?php echo $parts['prod_id']; ?></td>
+											            <td><?php echo $parts['part_amt']; ?></td>
 											            <td class="text-center td-actions">
-												            <a rel="tooltip" href="part.php?editrole=<?php echo $part['part_id']; ?>" class="btn btn-success btn-link" title="Edit">
+												            <a rel="tooltip" href="edit_parts.php?edit_parts=<?php echo $parts['part_id']; ?>" class="btn btn-success btn-link" title="Edit">
 												              <i class="material-icons">edit</i>
 												            </a>
 												        </td><td>
-												            <a rel="tooltip" href="process/admin/part_process.php?delrole=<?php echo $part['part_id']; ?>" class="btn btn-danger btn-link" title="Copy">
+												            <a rel="tooltip" href="copy_parts.php?copy_parts=<?php echo $parts['part_id']; ?>" class="btn btn-danger btn-link" title="Copy">
 												              <i class="material-icons">file_copy</i>
 												            </a>
 											            </td>
 											            <td>
-												            <a rel="tooltip" href="process/admin/part_process.php?delrole=<?php echo $part['part_id']; ?>" class="btn btn-danger btn-link" title="Delete">
+												            <a rel="tooltip" href="del_parts.php?del_parts=<?php echo $parts['part_id']; ?>" class="btn btn-danger btn-link" title="Delete">
 												              <i class="material-icons">close</i>
 												            </a>
 											            </td>
